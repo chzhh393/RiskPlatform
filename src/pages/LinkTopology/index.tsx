35bc0ea739c553d0ea95242cdf52a87035b97128
shulie <luohan@shulie.io>
2024-12-16 18:17:44 +0800

    解决部署失败1817

--- src/pages/LinkTopology/index.tsx
+++ src/pages/LinkTopology/index.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { Layout } from 'antd';
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import BusinessTree from './components/BusinessTree';
-import TopologyGraph from './components/TopologyGraph';
+import { BusinessTree } from './components/BusinessTree';
+import { TopologyGraph } from './components/TopologyGraph';
+import { mockBusinessData } from './services/api';
 import './styles.less';
 
 const { Sider, Content } = Layout;
 
 const LinkTopologyPage: React.FC = () => {
-  const [selectedLink, setSelectedLink] = useState<string>();
+  const [selectedLinkId, setSelectedLinkId] = useState<string>();
   const [collapsed, setCollapsed] = useState(false);
 
   const toggleSider = () => {
@@ -25,13 +26,16 @@ const LinkTopologyPage: React.FC = () => {
         collapsed={collapsed}
         trigger={null}
       >
-        <BusinessTree onSelect={setSelectedLink} />
+        <BusinessTree 
+          data={mockBusinessData}
+          onSelect={setSelectedLinkId} 
+        />
         <div className="sider-trigger" onClick={toggleSider}>
           {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
         </div>
       </Sider>
       <Content className="right-content">
-        <TopologyGraph linkId={selectedLink} />
+        <TopologyGraph linkId={selectedLinkId} />
       </Content>
     </Layout>
   );
