46b20a48e53fddcdef26766e282c1b50b941cbe5
shulie <luohan@shulie.io>
2024-12-16 19:53:48 +0800

    修复访问不了

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LinkTopology from './pages/LinkTopology';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <LinkTopology />
+    </div>
+  );
+};
+
+export default App; 
